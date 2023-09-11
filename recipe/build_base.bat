7e56b661e11a1098a12e79f7326c7f83ac33a914
Serhii Kupriienko <skupriienko@anaconda.com>
2023-09-11 11:21:06 +0300

    Debug entry_points

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,6 +1,1 @@
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
-
-:: To rewrite precommit.conf contents per conda environment
-if not exist %PREFIX%\Scripts mkdir %PREFIX%\Scripts
-copy "%RECIPE_DIR%\write_precommitconf.bat" "%PREFIX%\Scripts\.precommit-post-link.bat"
-if %errorlevel% neq 0 exit /b %errorlevel%
